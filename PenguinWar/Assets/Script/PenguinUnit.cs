f1353f8b4407ec6b4b97ba319208def48e363a4e
hongjeongyun <dreamcastrar@gmail/com>
2017-04-25 20:08:50 +0900

    replace

---
+++ PenguinWar/Assets/Script/PenguinUnit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinUnit : MonoBehaviour
+{
+	void Start ()
+    {
+        StartCoroutine(PenguinUnitUpdate());
+	}
+
+    IEnumerator PenguinUnitUpdate()
+    {
+        while(true)
+        {
+            yield return null;
+            //Debug.Log("PENGUIN_IS_ALIVE");
+            RaycastHit hit = GameObject.Find("Player").GetComponentInChildren<Cam>().rayHit;
+
+            if(hit.collider == null)
+            {
+                Debug.Log("OK");
+            }
+
+
+            if (hit.collider == this.gameObject.GetComponent<Collider>())
+            {
+                //Debug.Log("OK");
+            }
+        }
+    }
+}
